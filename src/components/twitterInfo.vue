b17aed2bdcbdf424db691b2dc51e374269a4be10
satinder <satindersingh472@gmail.com>
2022-08-09 20:13:34 -0600

    finished

---
+++ src/components/twitterInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p id="box"> </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        mounted(){
+            let box = document.getElementById(`box`);
+            box[`innerHTML`] += `<h2>${this[`num_friends`]} </h2>`;
+            for(let i =0;i<this[`tweets`].length;i++){
+            box[`innerHTML`] += `<p>tweet is ${this[`tweets`][i]}</p>`;
+            }
+        },
+        data() {
+            return {
+                tweets: [`be humble`,`be positive`,`be honest`],
+                num_friends: 100,
+                logged_in: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
